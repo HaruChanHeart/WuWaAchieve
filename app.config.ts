de6d0f373ee29ce711cd829a38f79b9643538fcd
Haru <haruchan0111@naver.com>
2024-06-02 10:51:48 +0900

    SSR Update

--- app.config.ts
+++ app.config.ts
@@ -1,8 +1,8 @@
 import { defineConfig } from "@solidjs/start/config";
 
 export default defineConfig({
-    ssr: false,
+    ssr: true,
     server: {
-        "preset": "vercel-static"
+        "preset": "vercel"
     }
 });
