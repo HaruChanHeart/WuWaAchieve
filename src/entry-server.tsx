17a0d5b472049475ad6fdc14f49e68b101fb1fa6
Haru <haruchan0111@naver.com>
2024-06-02 14:32:30 +0900

    Metadata Update

--- src/entry-server.tsx
+++ src/entry-server.tsx
@@ -8,6 +8,16 @@ export default createHandler(() => (
         <head>
           <meta charset="utf-8" />
           <meta name="viewport" content="width=device-width, initial-scale=1" />
+          <meta property="description" content="Wuthering Waves Achievements DB" />
+          <meta property="twitter:image" content="/thumbnail.webp" />
+          <meta property="twitter:card" content="/thumbnail.webp" />
+          <meta property="twitter:title" content="WuWaAchieve" />
+          <meta property="twitter:description" content="Wuthering Waves Achievements DB" />
+          <meta property="og:image" content="/thumbnail.webp" />
+          <meta property="og:site_name" content="WuWaAchieve" />
+          <meta property="og:title" content="WuWaAchieve" />
+          <meta property="og:description" content="Wuthering Waves Achievements DB" />
+          <meta property="og:url" content="https://wuwa-achieve.vercel.app" />
           <link rel="apple-touch-icon" type="180x180" href="/apple-touch-icon.png" />
           <link rel="icon" type="image/png" href="/favicon-32x32.png" />
           <link rel="icon" type="image/png" href="/favicon-16x16.png" />
